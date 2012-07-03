7956c3ee336dc1441931b1bce8d4e3ec37234f26
Shea Dev <shea.dev.v3@gmail.com>
2012-07-02 19:08:15 -0700

    HTML5 Boilerplate - starting

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>		<html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>		<html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+
+	<!-- Use the .htaccess and remove these lines to avoid edge case issues.
+			 More info: h5bp.com/i/378 -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>IHS Class of 2015's Facebook app</title>
+	<meta name="description" content="">
+
+	<!-- Mobile viewport optimized: h5bp.com/viewport -->
+	<meta name="viewport" content="width=device-width">
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
+
+	<!-- All JavaScript at the bottom, except this Modernizr build.
+			 Modernizr enables HTML5 elements & feature detects for optimal performance.
+			 Create your own custom Modernizr build: www.modernizr.com/download/ -->
+	<script src="js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+	<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
+			 chromium.org/developers/how-tos/chrome-frame-getting-started -->
+	<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+	<header>
+		<img id="banner" src="media/banner.png">
+		<nav>
+			<a href="/tshirt-vote.php"><div class="navLink">Class T-shirt Design Vote</div></a>
+			<a href="/tshirt-vote.php"><div class="navLink">Class T-shirt Design Vote</div></a>
+		</nav>
+	</header>
+	<div role="main">
+
+	</div>
+	<footer>
+		<p>Your use of this site is subject to the following terms and conditions. If you do not wish to agree to them, do not access the site.</p>
+		<p>The owners of this site, Facebook, and FUSD will not be held liable for any damages either directly or indirectly resulting from your use of this site, nor will they be held responsible for any actions that you take through the site's interface. We do not condone the posting of stolen, copyrighted, or innapropriate work. This site is provided to you with no warranty or guarantees whatsoever, even that of fitness for a particular purpose. Use at your own risk. We do not authorize the reproduction, storage, or transmission of any component of our site as they are all @copy; 2012. We reserve the right to collect any information about you which either your browser or you send to our servers. This information may include, but is not limited to, your IP address, your browser information, your computer information, your location, and any text that you submit through the site. We may store and redistribute this information as we please. By submitting anything to the site (eg: text, votes, ect), you transfer all rights to it to us. Any information that we access through the Facebook API about you will not be stored, and will be used in accordance with Facebook's terms of service.
+	</footer>
+
+
+	<!-- JavaScript at the bottom for fast page loading -->
+
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+	<!-- scripts concatenated and minified via build script -->
+	<script src="js/plugins.js"></script>
+	<script src="js/script.js"></script>
+	<!-- end scripts -->
+
+	<!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
+			 mathiasbynens.be/notes/async-analytics-snippet -->
+	<script>
+		var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+		s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
+</body>
+</html>
